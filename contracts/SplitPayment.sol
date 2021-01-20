9859d78b5400e3aab8ab1eb6741ac9a0cdaf9e29
jeffrey <jeffreyassan88@gmail.com>
2021-01-19 21:03:54 -0500

    Split Payment Project Completed

---
+++ contracts/SplitPayment.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.0;
+
+contract SplitPayment {
+
+  address owner;
+  
+  constructor(address _owner) public  {
+
+      owner = _owner; 
+  }
+  
+  function send(address payable[] memory to, uint[] memory amount ) payable public ownerOnly {
+
+    require(to.length == amount.length, 'to must be same length as amount');
+
+    for(uint i = 0; i < to.length; i++) {
+
+      to[i].transfer(amount[i]);
+    }
+  }
+  
+  modifier ownerOnly() {
+
+    require(msg.sender == owner,'Will not send if it is not owner');
+    _;
+  }
+}
